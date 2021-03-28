779c68ec7a2449107a235938069e5755292c4b61
FlashDrive.io <73052948+flashdriveio@users.noreply.github.com>
2021-03-27 20:48:19 -0400

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(msg.encode())
 
 
-port = int(os.getenv('PORT', 80))
+port = int(os.getenv('PORT', 8000))
 print('Listening on port %s' % (port))
 httpd = socketserver.TCPServer(('', port), Handler)
 httpd.serve_forever()
